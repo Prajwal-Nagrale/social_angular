a79ad9292c560f9a7573630cbc18363609c11211
Prajwal Nagrale <prajwalnagrale@gmail.com>
2021-04-14 12:15:33 +0530

    update

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { MemberComponent } from './member/member.component';
+import { GroupsComponent } from './groups/groups.component';
+import { PhotosComponent } from './photos/photos.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'member',component:MemberComponent},
+  {path:'groups',component:GroupsComponent},
+  {path:'photos',component:PhotosComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'',redirectTo:'/home', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { MemberComponent } from './member/member.component';
+import { GroupsComponent } from './groups/groups.component';
+import { PhotosComponent } from './photos/photos.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignupComponent,
+    NavbarComponent,
+    HomeComponent,
+    MemberComponent,
+    GroupsComponent,
+    PhotosComponent,
+    ProfileComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
